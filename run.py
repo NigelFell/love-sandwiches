7d8a2f244c7aaa8f2ae7bf7f89f32ebbc93768f1
NigelFell <nigelfell@yahoo.co.uk>
2023-03-14 14:53:17 +0000

    feat: added funtion to return sales data for the last 5 markets in columns

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 # data = sales.get_all_values()
 # print(data)
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
@@ -56,6 +57,7 @@ def validate_data(values):
 
     return True
 
+
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
@@ -65,6 +67,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -84,6 +87,23 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -96,4 +116,7 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
+pprint(sales_columns)
